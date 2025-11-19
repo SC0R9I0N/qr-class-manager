6e8fba3285ffeff8c1078394d8de1fb97980b0c0
SC0R9I0N <167116668+SC0R9I0N@users.noreply.github.com>
2025-11-19 14:07:54 -0500

    Update infra_stack.py

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -7,11 +7,11 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class InfraStack(Stack):
+class QrClassManagerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # DynamoDB table
+        # DynamoDB table for attendance
         attendance_table = ddb.Table(
             self, "AttendanceTable",
             partition_key={"name": "sessionId", "type": ddb.AttributeType.STRING},
@@ -19,7 +19,7 @@ class InfraStack(Stack):
             billing_mode=ddb.BillingMode.PAY_PER_REQUEST
         )
 
-        # S3 bucket
+        # S3 bucket for resource uploads
         resource_bucket = s3.Bucket(
             self, "ResourceBucket",
             versioned=True,
@@ -27,7 +27,7 @@ class InfraStack(Stack):
             auto_delete_objects=True
         )
 
-        # Lambda stub
+        # Lambda: log_attendance
         log_attendance_lambda = _lambda.Function(
             self, "LogAttendanceLambda",
             runtime=_lambda.Runtime.PYTHON_3_11,
@@ -39,10 +39,61 @@ class InfraStack(Stack):
             }
         )
 
+        # Lambda: upload_resource
+        upload_resource_lambda = _lambda.Function(
+            self, "UploadResourceLambda",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="upload_resource.handler",
+            code=_lambda.Code.from_asset("../lambdas"),
+            environment={
+                "BUCKET_NAME": resource_bucket.bucket_name
+            }
+        )
+
+        # Lambda: get_stats
+        get_stats_lambda = _lambda.Function(
+            self, "GetStatsLambda",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="get_stats.handler",
+            code=_lambda.Code.from_asset("../lambdas"),
+            environment={
+                "TABLE_NAME": attendance_table.table_name
+            }
+        )
+
+        # Lambda: get_resource
+        get_resource_lambda = _lambda.Function(
+            self, "GetResourceLambda",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="get_resource.handler",
+            code=_lambda.Code.from_asset("../lambdas"),
+            environment={
+                "BUCKET_NAME": resource_bucket.bucket_name
+            }
+        )
+
+        # Grant permissions
         attendance_table.grant_read_write_data(log_attendance_lambda)
+        attendance_table.grant_read_data(get_stats_lambda)
         resource_bucket.grant_read_write(log_attendance_lambda)
+        resource_bucket.grant_read_write(upload_resource_lambda)
+        resource_bucket.grant_read(get_resource_lambda)
 
         # API Gateway
         api = apigw.RestApi(self, "ClassBitsApi", rest_api_name="ClassBits Service")
+
+        # POST /attendance
         attendance = api.root.add_resource("attendance")
         attendance.add_method("POST", apigw.LambdaIntegration(log_attendance_lambda))
+
+        # POST /resource
+        resource = api.root.add_resource("resource")
+        resource.add_method("POST", apigw.LambdaIntegration(upload_resource_lambda))
+
+        # GET /stats
+        stats = api.root.add_resource("stats")
+        stats.add_method("GET", apigw.LambdaIntegration(get_stats_lambda))
+
+        # GET /resource/{id}
+        resource_id = resource.add_resource("{id}")
+        resource_id.add_method("GET", apigw.LambdaIntegration(get_resource_lambda))
